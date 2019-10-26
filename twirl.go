5a684634745d21c205f49503d6ff8bdf0e12f45c
John Dreystadt <jdreystadt@hotmail.com>
2019-10-26 14:26:06 -0400

    Initial entry with logging, signals, heartbeat

---
+++ twirl.go
@@ -0,0 +1,88 @@
+// This is the main file for the dns-twirl program. This program is just a
+// simple DNS repeater that uses a simple file of local addresses to provide
+// name lookups for the local network.
+//
+// The purpose of this program is mostly just to be an example for later
+// work.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var myLogger *log.Logger
+
+type messageType interface {
+	msgType() string
+}
+
+type signalMessage struct {
+	signalValue os.Signal
+}
+
+func (p *signalMessage) msgType() string {
+	return "signal"
+}
+
+type heartbeatMessage struct {
+	hbValue time.Time
+}
+
+func (p *heartbeatMessage) msgType() string {
+	return "heartbeat"
+}
+
+func main() {
+
+	// Setup communication channels
+
+	responseChan := make(chan messageType, 1)
+
+	myLogger = log.New(os.Stderr, "faux:", log.Lshortfile|log.LUTC|log.LstdFlags)
+	_ = myLogger.Output(1, "Starting")
+
+	go signalCatcher(responseChan)
+
+	go heart(responseChan)
+
+	// Wait for message on a channel
+
+mainLoop:
+	for true {
+		select {
+		case message := <-responseChan:
+			_ = myLogger.Output(1, fmt.Sprint("Got response - ", message.msgType()))
+			if message.msgType() == "signal" {
+				break mainLoop
+			}
+		}
+	}
+
+	_ = myLogger.Output(1, "Ending")
+
+}
+
+func signalCatcher(resChan chan messageType) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	for true {
+		caughtSignal := <-signalChan
+		message := new(signalMessage)
+		message.signalValue = caughtSignal
+		resChan <- message
+	}
+}
+
+func heart(resChan chan messageType) {
+	for true {
+		time.Sleep(1 * time.Second)
+		message := new(heartbeatMessage)
+		message.hbValue = time.Now()
+		resChan <- message
+	}
+}
