a20884543cc8c00adb3612d6af39e013034cfdaf
John Dreystadt <jdreystadt@hotmail.com>
2019-11-06 18:29:44 -0500

    Add beginings of the dns server itself

--- twirl.go
+++ twirl.go
@@ -10,10 +10,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/signal"
 	"time"
+
+	"golang.org/x/net/dns/dnsmessage"
 )
+
+const dnsPort = ":1053"
 
 var myLogger *log.Logger
 
@@ -37,11 +42,20 @@ func (p *heartbeatMessage) msgType() string {
 	return "heartbeat"
 }
 
+type statusMessage struct {
+	statusValue string
+}
+
+func (p *statusMessage) msgType() string {
+	return "status"
+}
+
 func main() {
 
 	// Setup communication channels
 
-	responseChan := make(chan messageType, 1)
+	var responseChan = make(chan messageType, 1)
+	var dnsControlChan = make(chan messageType, 1)
 
 	myLogger = log.New(os.Stderr, "faux:", log.Lshortfile|log.LUTC|log.LstdFlags)
 	_ = myLogger.Output(1, "Starting")
@@ -50,13 +64,28 @@ func main() {
 
 	go heart(responseChan)
 
+	go dnsServer(dnsControlChan, responseChan)
+
 	// Wait for message on a channel
 
 mainLoop:
 	for true {
 		select {
 		case message := <-responseChan:
-			_ = myLogger.Output(1, fmt.Sprint("Got response - ", message.msgType()))
+			switch message.msgType() {
+			case "signal":
+				break mainLoop
+			case "heartbeat":
+				select {
+				case dnsControlChan <- message:
+				default:
+					_ = myLogger.Output(1, "Control channel is blocked")
+				}
+				dnsControlChan <- message
+			case "status":
+				var statusMsg = message.(*statusMessage)
+				_ = myLogger.Output(1, fmt.Sprint("Got ", statusMsg.statusValue))
+			}
 			if message.msgType() == "signal" {
 				break mainLoop
 			}
@@ -70,6 +99,7 @@ mainLoop:
 func signalCatcher(resChan chan messageType) {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
+	signal.Notify(signalChan, os.Kill)
 	for true {
 		caughtSignal := <-signalChan
 		message := new(signalMessage)
@@ -80,9 +110,80 @@ func signalCatcher(resChan chan messageType) {
 
 func heart(resChan chan messageType) {
 	for true {
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 		message := new(heartbeatMessage)
 		message.hbValue = time.Now()
 		resChan <- message
+	}
+}
+
+func dnsServer(controlChan, resChan chan messageType) {
+	// Open the UDP port
+	pc, err := net.ListenPacket("udp", dnsPort)
+	if err != nil {
+		return
+	}
+	// Close down when exiting
+	defer pc.Close()
+	// Create a buffer to hold the received messages
+	var buffer = make([]byte, 65536)
+
+mainLoop:
+	for true {
+		var deadline = time.Now().Add(1 * time.Second)
+		err = pc.SetReadDeadline(deadline)
+		if err != nil {
+			_ = myLogger.Output(1, "Can't set a deadline")
+			return
+		}
+		// Finally time to read
+		n, addr, err := pc.ReadFrom(buffer)
+		if err == nil {
+			_ = myLogger.Output(1, fmt.Sprint("Got a buffer of length ", n, " from addr ", addr))
+			var p dnsmessage.Parser
+			if _, err := p.Start(buffer); err != nil {
+				_ = myLogger.Output(1, fmt.Sprint("Parser did not Start ", err.Error()))
+			}
+			for {
+				q, err := p.Question()
+				if err == dnsmessage.ErrSectionDone {
+					break
+				}
+				if err != nil {
+					_ = myLogger.Output(1, fmt.Sprint("Error on Question ", err.Error()))
+					break
+				}
+				_ = myLogger.Output(1, fmt.Sprint("Got a question ", q.Name.String()))
+			}
+		} else {
+			// Got an error but this might be a timeout.
+			nerr, ok := err.(net.Error)
+			if !ok {
+				// Type assertion failed
+				_ = myLogger.Output(1, "Type assertion failure")
+				return
+			}
+			if !nerr.Timeout() {
+				// Error found that is not a timeout
+				_ = myLogger.Output(1, fmt.Sprint("Error ", err.Error()))
+				return
+			}
+		}
+	drainControlChannel:
+		for {
+			select {
+			case controlMsg := <-controlChan:
+				switch controlMsg.msgType() {
+				case "heartbeat":
+					var message = new(statusMessage)
+					message.statusValue = "Heartbeat on control channel"
+					resChan <- message
+				case "signal":
+					break mainLoop
+				}
+			default:
+				break drainControlChannel
+			}
+		}
 	}
 }
